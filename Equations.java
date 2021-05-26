bd14955ba120ae6465050ba2e61d3d8f63c76f49
Sunny Adhikari <sunnyadh07@gmail.com>
2021-05-25 21:13:52 -0700

    First commit

---
+++ Equations.java
@@ -0,0 +1,14 @@
+public class Equations {
+    //public static double hypotenuseOfTrianle(double a, double b, double c){
+        //return ((a^2 + b^2) = c^2);
+        //return Math.pow(a)
+
+
+    public static double areaOfTriangle(double h, double b){
+        return ((h * b) / 2);
+    }
+    public static double circumferenceOfSphere(double r) {
+        return Math.PI * (r * r);
+    }
+
+}
---
+++ EquationsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquationsTest {
+
+    @Test
+    void areaOfTriangle() {
+        assertEquals(2, Equations.areaOfTriangle(2,2));
+
+    }
+    @Test
+    void circumferenceOfSphere(){
+        assertEquals(78.5, 5.0);
+    }
+
+}
